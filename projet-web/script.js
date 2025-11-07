6aa377f0d6a926b2f2ad4c38a3349b08a318bdee
DevMaxoDev <124282256+DevMaxoDev@users.noreply.github.com>
2025-11-07 14:59:10 +0000

    feat: Implémentation du toggle dark mode

--- projet-web/script.js
+++ projet-web/script.js
@@ -1,7 +1,18 @@
 console.log("Script chargé !")
 
 const titrePrincipal = document.querySelector('#titre-principal');
-titrePrincipal.textContent = "Vous avez cliqué sur le titre !";
+
 titrePrincipal.addEventListener('click', () => {
+  titrePrincipal.textContent = "Vous avez cliqué sur le titre !";
+  console.log('Bouton cliqué !');
+});
+
+
+
+
+const themeButton = document.querySelector('#theme-toggle');
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode')
   console.log('Bouton cliqué !');
 });
