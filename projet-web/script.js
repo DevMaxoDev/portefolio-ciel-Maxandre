54ee18ecaf0f6bebb7276df8e090b585938222d4
DevMaxoDev <124282256+DevMaxoDev@users.noreply.github.com>
2025-11-13 13:37:30 +0000

    feat: Debut du projet

--- projet-web/script.js
+++ projet-web/script.js
@@ -10,9 +10,15 @@ titrePrincipal.addEventListener('click', () => {
 
 
 
+
 const themeButton = document.querySelector('#theme-toggle');
 
 themeButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode')
+  if (document.body.classList.contains('dark-mode')){
+      themeButton.textContent = "White mode"
+  } else {
+      themeButton.textContent = "Dark mode"
+  }
   console.log('Bouton cliqué !');
 });
